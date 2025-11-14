47cd0885f914642f41f1dd4ca5d0f5a0107195ef
luciferprotean <avalontech@gmail.com>
2025-11-14 10:22:52 +0530

    added timestamp column

--- index.js
+++ index.js
@@ -51,11 +51,21 @@ app.post('/submit', async (req, res) => {
     const rows = await sheet.getRows();
     const exists = rows.some((r) => r.publicKey === match.chip_pk);
 
+    const now = new Date();
+    const timestamp =
+      now.getDate().toString().padStart(2, "0") + "/" +
+      (now.getMonth() + 1).toString().padStart(2, "0") + "/" +
+      now.getFullYear() + " " +
+      now.getHours().toString().padStart(2, "0") + ":" +
+      now.getMinutes().toString().padStart(2, "0") + ":" +
+      now.getSeconds().toString().padStart(2, "0");
+
     if (!exists) {
       await sheet.addRow({
         serial: match.serial_number,
         type: match.tier_code,
         publicKey: match.chip_pk,
+        timestamp: timestamp
       });
       console.log(`Added ${match.chip_pk} to Google Sheet`);
     }
